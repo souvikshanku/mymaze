3ab2b22908f92fe5eb9e8c62c0df82ebb832c0e0
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-08-12 20:23:40 +0530

    plotting maze is now possible

--- src/grid.py
+++ src/grid.py
@@ -19,3 +19,28 @@ class Grid:
             nbrs.append((x, y + 1))
 
         return nbrs
+
+
+def plot(visited, size):
+
+    size = 2 * size - 1
+    maze = np.zeros((size, size))
+
+    path = [
+        (cell[0] * 2, cell[1] * 2) for cell in visited
+    ]
+
+    for i in range(len(path) - 1):
+        x1, y1 = path[i]
+        x2, y2 = path[i + 1]
+
+        maze[size - 1 - y1, x1] = 1
+        maze[size - 1 - y2, x2] = 1
+
+        maze[size - 1 - (y1+y2)//2, (x1+x2)//2] = 1
+
+    plt.imshow(np.pad(maze, 1))
+    plt.gca().axis('off')
+
+
+plot(visited, m.size)
